af34a15b95206a0d4c4547841aac1bbc7555e3f2
augggievon <aug2148351@maricopa.edu>
2026-05-12 09:59:20 -0700

    aggregator finished

--- aggregator/src/main/java/com/example/aggregator/client/AggregatorRestClient.java
+++ aggregator/src/main/java/com/example/aggregator/client/AggregatorRestClient.java
@@ -1,8 +1,13 @@
 package com.example.aggregator.client;
 
 import com.example.aggregator.model.Entry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class AggregatorRestClient {
@@ -20,6 +25,78 @@ public class AggregatorRestClient {
         Entry result = restTemplate.getForObject(uri, Entry.class);
 
         return result;
-
     }
+
+    /**
+     * Get Words Starting With
+     *
+     * @param chars
+     * @return
+     */
+    public List<Entry> getWordsStartingWith(String chars) {
+
+        // Get URL
+        String uri = "http://localhost:9091/getWordsStartingWith/" + chars;
+
+        // Call endpoint
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+
+        // Convert body to array
+        Entry[] entryArray = result.getBody();
+
+        // Return list of Entries
+        return Arrays.stream(entryArray)
+                .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsThatContain(String chars) {
+
+        // Get URL
+        String uri = "http://localhost:9091/getWordsThatContain/" + chars;
+
+        // Call endpoint
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+
+        // Convert body to array
+        Entry[] entryArray = result.getBody();
+
+        // Return list of Entries
+        return Arrays.stream(entryArray)
+                .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsThatContainConsecutiveLetters() {
+
+        // Get URL
+        String uri = "http://localhost:9091/getWordsThatContainConsecutiveLetters";
+
+        // Call endpoint
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+
+        // Convert body to array
+        Entry[] entryArray = result.getBody();
+
+        // Return list of Entries
+        return Arrays.stream(entryArray)
+                .collect(Collectors.toList());
+    }
+
+    public List<Entry> getWordsEndingWith(String chars) {
+
+        // Get URL
+        String uri = "http://localhost:9091/getWordsEndingWith/" + chars;
+
+        // Call endpoint
+        ResponseEntity<Entry[]> result = restTemplate.getForEntity(uri, Entry[].class);
+
+        // Convert body to array
+        Entry[] entryArray = result.getBody();
+
+        // Return list of Entries
+        return Arrays.stream(entryArray)
+                .collect(Collectors.toList());
+    }
+
+
 }
+
--- aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
+++ aggregator/src/main/java/com/example/aggregator/controller/AggregatorController.java
@@ -2,15 +2,21 @@ package com.example.aggregator.controller;
 
 import com.example.aggregator.model.Entry;
 import com.example.aggregator.service.AggregatorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AggregatorController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AggregatorController.class);
 
     private AggregatorService aggregatorService;
 
@@ -30,4 +36,62 @@ public class AggregatorController {
     public Entry getDefinitionFor(@PathVariable String word) {
         return aggregatorService.getDefinitionFor(word);
     }
+
+    @GetMapping("/getWordsThatContainSuccessiveLettersAndStarsWith/{chars}")
+    public List<Entry> getWordsThatContainSuccessiveLettersAndStarsWith(@PathVariable String chars) {
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        List<Entry> entries = aggregatorService.getWordsThatContainSuccessiveLettersAndStartsWith(chars);
+        stopWatch.stop();
+
+        long nanoSeconds = stopWatch.getLastTaskTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for words starting with [")
+                .append(chars)
+                .append("] and containing consecutive letters, containing ")
+                .append(entries.size())
+                .append(" entries in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+
+        logger.info(message);
+
+        return entries;
+
+    }
+
+    @GetMapping("/getWordsThatContainSuccessiveLettersAndContains/{chars}")
+    public List<Entry> getWordsThatContainSuccessiveLettersAndContains(@PathVariable String chars) {
+
+        StopWatch stopWatch = new StopWatch();
+
+        stopWatch.start();
+        List<Entry> entries = aggregatorService.getWordsThatContainSuccessiveLettersAndContains(chars);
+        stopWatch.stop();
+
+        long nanoSeconds = stopWatch.getLastTaskTimeNanos();
+        String message = new StringBuilder().append("Retrieved entries for words containing [")
+                .append(chars)
+                .append("] and containing consecutive letters, containing ")
+                .append(entries.size())
+                .append(" entries in ")
+                .append(nanoSeconds / 1000000.0)
+                .append("ms")
+                .toString();
+
+        logger.info(message);
+
+        return entries;
+
+    }
+
+    @GetMapping("/getAllPalindromes")
+    public List<Entry> getAllPalindromes() {
+        return aggregatorService.getAllPalindromes();
+    }
+
+
 }
+
